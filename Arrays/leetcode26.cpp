4d07fb3d7ee6f10a6116d3549f4932be60263fb3
Mridul Singh <91845185+Mridul-Singh-01@users.noreply.github.com>
2024-07-01 00:29:49 +0530

    Array Questions added

---
+++ Arrays/leetcode26.cpp
@@ -0,0 +1,32 @@
+// Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+// Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int i=0;
+        for(int j=1;j<nums.size();j++){
+            if(nums[j]!=nums[i]){
+                nums[i+1]=nums[j];
+                i++;
+            }
+        }
+        return i+1;
+    }
+};
+
+int main(){
+    Solution solution;
+    vector<int> arr;
+    arr.push_back(1);
+    arr.push_back(1);
+    arr.push_back(2);
+    arr.push_back(2);
+    arr.push_back(2);
+    arr.push_back(3);
+    cout << solution.removeDuplicates(arr);
+    return 0;
+}
