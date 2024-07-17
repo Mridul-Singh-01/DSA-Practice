338f244861b45f8fff52fe7da185e27975239781
Mridul Singh <91845185+Mridul-Singh-01@users.noreply.github.com>
2024-07-17 23:09:35 +0530

    changes

--- Arrays/leetcode229.cpp
+++ Arrays/leetcode229.cpp
@@ -1,3 +1,11 @@
+// Given an integer array of size n, find all elements that appear more than ⌊ n/3 ⌋ times.
+// Example 1:
+// Input: nums = [3,2,3]
+// Output: [3]
+// Example 2:
+// Input: nums = [1]
+// Output: [1]
+
 #include<bits/stdc++.h>
 using namespace std;
 
