8d95063ca333f5da21876f013f5a39cb332ccae3
Mridul Singh <91845185+Mridul-Singh-01@users.noreply.github.com>
2024-07-17 23:41:40 +0530

    new questions

---
+++ Strings/leetcode1903.cpp
@@ -0,0 +1,23 @@
+// You are given a string num, representing a large integer. Return the largest-valued odd integer (as a string) that is a non-empty substring of num, or an empty string "" if no odd integer exists.
+// A substring is a contiguous sequence of characters within a string.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string largestOddNumber(string num) {
+        for (int i = num.size() - 1; i >= 0; i--) {
+            if ((num[i] - '0') % 2 != 0) {
+                return num.substr(0, i + 1);
+            }
+        }
+        return "";
+    }
+};
+
+int main(){
+    Solution s;
+    cout << s.largestOddNumber("52434442");
+    return 0;
+}
