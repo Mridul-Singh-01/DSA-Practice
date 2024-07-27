bc678a648daa9ac7ae36ffe75ba392da0b10508c
Mridul-Singh-01 <mridulbbk@gmail.com>
2024-07-28 00:45:44 +0530

    changes

--- LinkedList/leetcode237.cpp
+++ LinkedList/leetcode237.cpp
@@ -2,20 +2,50 @@
 // You are given the node to be deleted node. You will not be given access to the first node of head.
 // All the values of the linked list are unique, and it is guaranteed that the given node node is not the last node in the linked list.
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-struct ListNode{
-    int data;
+struct ListNode {
+    int val;
     ListNode* next;
-}
+    ListNode(int x) : val(x), next(nullptr) {}
+};
 
 class Solution {
 public:
     void deleteNode(ListNode* node) {
-        node->val=node->next->val;
-        ListNode* temp=node->next;
-        node->next=node->next->next;
+        node->val = node->next->val;
+        ListNode* temp = node->next;
+        node->next = node->next->next;
         delete temp;
     }
 };
+
+void printList(ListNode* head) {
+    ListNode* current = head;
+    while (current != nullptr) {
+        cout << current->val << " ";
+        current = current->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    ListNode* head = new ListNode(4);
+    head->next = new ListNode(5);
+    head->next->next = new ListNode(1);
+    head->next->next->next = new ListNode(9);
+    cout << "Original list: ";
+    printList(head);
+    Solution solution;
+    solution.deleteNode(head->next);
+    cout << "List after deleting node with value 5: ";
+    printList(head);
+    ListNode* current = head;
+    while (current != nullptr) {
+        ListNode* next = current->next;
+        delete current;
+        current = next;
+    }
+    return 0;
+}
