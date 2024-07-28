c50baaa60ef22e81d8a3e8bd7c808877eeaabe28
Mridul-Singh-01 <mridulbbk@gmail.com>
2024-07-29 00:36:50 +0530

    changes

---
+++ LinkedList/Reverse.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+class Solution
+{
+public:
+    ListNode *reverseList(ListNode *head)
+    {
+        ListNode *temp = head;
+        ListNode *front = nullptr;
+        ListNode *prev = nullptr;
+        while (temp != nullptr)
+        {
+            front = temp->next;
+            temp->next = prev;
+            prev = temp;
+            temp = front;
+        }
+        return prev;
+    }
+    void printLL(ListNode *head)
+    {
+        while (head != nullptr)
+        {
+            cout << head->val << " ";
+            head = head->next;
+        }
+        cout << "\n";
+    }
+};
+
+int main()
+{
+    Solution s;
+    ListNode *head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head = s.reverseList(head);
+    s.printLL(head);
+    return 0;
+}
