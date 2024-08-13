60677cc9153ea5b9833cb8114c59c5a1129cc705
Mridul-Singh-01 <mridulbbk@gmail.com>
2024-08-13 18:50:57 +0530

    changes

--- LinkedList/DeleteKInDLL.cpp
+++ LinkedList/DeleteKInDLL.cpp
@@ -1,46 +1,79 @@
-// Delete all occurrences of a given key in a doubly linked list - > GFG
-// You are given the head_ref of a doubly Linked List and a Key.
+// Delete all occurrences of a given key in a doubly linked list
+// You are given the head_ref of a doubly Linked List and a Key. 
 // Your task is to delete all occurrences of the given key if it is present and return the new DLL.
 
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
+struct Node {
+    int data;
+    Node* next;
+    Node* prev;
+    Node(int x) : data(x), next(nullptr), prev(nullptr) {}
+};
 
-void deleteAllOccurOfX(struct Node **head_ref, int x)
-{
-    // Write your code here
+void append(Node** head_ref, int new_data) {
+    Node* new_node = new Node(new_data);
+    Node* last = *head_ref;
+    
+    if (*head_ref == nullptr) {
+        *head_ref = new_node;
+        return;
+    }
+    while (last->next != nullptr) {
+        last = last->next;
+    }
+    last->next = new_node;
+    new_node->prev = last;
+}
+
+void deleteAllOccurOfX(Node **head_ref, int x) {
     Node *temp = *head_ref;
-    while (temp != nullptr)
-    {
-        if (temp->data == x)
-        {
+    while (temp != nullptr) {
+        if (temp->data == x) {
             Node *toDelete = temp;
-            if (temp == *head_ref)
-            {
+            if (temp == *head_ref) {
                 *head_ref = temp->next;
-                if (*head_ref != nullptr)
-                {
+                if (*head_ref != nullptr) {
                     (*head_ref)->prev = nullptr;
                 }
-            }
-            else
-            {
-                if (temp->prev != nullptr)
-                {
+            } else {
+                if (temp->prev != nullptr) {
                     temp->prev->next = temp->next;
                 }
-                if (temp->next != nullptr)
-                {
+                if (temp->next != nullptr) {
                     temp->next->prev = temp->prev;
                 }
             }
-            free(toDelete);
+            temp = temp->next;
+            delete toDelete;
+        } else {
+            temp = temp->next;
         }
-        temp = temp->next;
     }
 }
 
-int main()
-{
-    
+void printList(Node* node) {
+    while (node != nullptr) {
+        cout << node->data << " ";
+        node = node->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    Node* head = nullptr;
+    append(&head, 1);
+    append(&head, 2);
+    append(&head, 2);
+    append(&head, 3);
+    append(&head, 4);
+    append(&head, 2);
+    append(&head, 5);
+    cout << "Original Doubly Linked List:" << endl;
+    printList(head);
+    int key = 2;
+    deleteAllOccurOfX(&head, key);
+    cout << "Doubly Linked List after deleting all occurrences of " << key << ":" << endl;
+    printList(head);
     return 0;
 }
