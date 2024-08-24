fb1cac44edefecde0e6e11a01bce1bb0b476a00c
Mridul-Singh-01 <mridulbbk@gmail.com>
2024-08-24 19:36:31 +0530

    new Question

--- Stack/NextGratestElement.cpp
+++ Stack/NextGratestElement.cpp
@@ -23,19 +23,15 @@ vector<int> nextGreaterElement(vector<int> arr){
 //Optimized Solution using Monotonic Stack(store elements in specific order in stack is called monotonic stack)
 vector<int> nextGreaterElementRightUsingStack(vector<int> arr) {
     int n = arr.size();
-    vector<int> res(n, -1);
+    vector<int> res(n);
     stack<int> st;
     for (int i = n - 1; i >= 0; i--) {
         while (!st.empty() && st.top() <= arr[i]) {
             st.pop();
         }
-        //Or we can do 
-        // if(st.empty())res[i]=-1;
-        // else{
-        //     res[i]=st.top();
-        // }
-        if (!st.empty()) {
-            res[i] = st.top();
+        if(st.empty())res[i]=-1;
+        else{
+            res[i]=st.top();
         }
         st.push(arr[i]);
     }
