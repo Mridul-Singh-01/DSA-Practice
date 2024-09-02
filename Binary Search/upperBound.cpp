05c5c02fd00bb841061530178b351fd0178794fd
Mridul-Singh-01 <mridulbbk@gmail.com>
2024-09-02 22:24:57 +0530

    new questions added

---
+++ Binary Search/upperBound.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int lowerBound(vector<int> arr,int x){
+    int n= arr.size();
+    int low=0;
+    int high=n-1;
+    int ans=-1;
+    while(low<=high){
+        int mid=low+(high-low)/2;
+        if(arr[mid]>x){
+            ans=mid;
+            high=mid-1;
+        }
+        else{
+            low=mid+1;
+        }
+    }
+    return ans;
+}
+
+int main(){
+    vector<int> arr={1,2,3,3,6,8,8,9,10};
+    cout << lowerBound(arr,0);
+    return 0;
+}
