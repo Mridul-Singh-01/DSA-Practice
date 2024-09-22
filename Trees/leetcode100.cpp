b61a5d83ab5fdf108b4dc3ba019c26db7950e377
Mridul-Singh-01 <mridulbbk@gmail.com>
2024-09-22 23:11:21 +0530

    new

---
+++ Trees/leetcode100.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+    Node(int x) {
+        data = x;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+class Solution {
+public:
+    bool isSameTree(Node* p, Node* q) {
+        if(p==nullptr && q==nullptr){
+            return true;
+        }
+        if(p==nullptr || q==nullptr){
+            return false;
+        }
+        if(p->val!=q->val){
+            return false;
+        }
+        bool a=isSameTree(p->left,q->left);
+        bool b=isSameTree(p->right,q->right);
+        return a&&b;
+    }
+};
