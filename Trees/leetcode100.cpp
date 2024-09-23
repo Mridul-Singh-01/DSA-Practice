24a1b296b0a34e468757e85a498e0fb1ee5365b9
Mridul-Singh-01 <mridulbbk@gmail.com>
2024-09-23 16:06:36 +0530

    new questions

--- Trees/leetcode100.cpp
+++ Trees/leetcode100.cpp
@@ -1,3 +1,7 @@
+//Same Tree
+//Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+//Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+
 #include<bits/stdc++.h>
 using namespace std;
 
