2a43c2566bf195fa7fd16848755ae70ffdeb05f7
Mridul-Singh-01 <mridulbbk@gmail.com>
2024-09-26 16:07:53 +0530

    new questions

---
+++ Interview150Questions/processLogs.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+vector<string>logAnalysis(vector<string> &logs){
+    int n=logs.size();
+    for(int i=0;i<n;i++){
+        string str=logs[i];
+        
+    }
+}
+
+int main(){
+    vector<string> arr={"88 99 200","99 88 400","99 32 100","12 12 400"};
+    logAnalysis(arr);
+    return 0;
+}
